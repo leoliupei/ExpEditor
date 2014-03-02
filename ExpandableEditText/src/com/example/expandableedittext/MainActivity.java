dad7b2794a4e90b1f16c4ddfdb15ff2a8106268f
Liu Pei <liupei@appchina.com>
2014-03-02 21:43:58 +0800

    first commit

---
+++ ExpandableEditText/src/com/example/expandableedittext/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.expandableedittext;
+
+import com.example.expandableedittext.R;
+
+import afzkl.development.colorpickerview.dialog.ColorPickerDialog;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends Activity {
+	
+	public int color = 0xFF000000;
+	public DroidWriterEditText dwEdit;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		ToggleButton boldToggle = (ToggleButton) findViewById(R.id.BoldButton);
+		ToggleButton italicsToggle = (ToggleButton) findViewById(R.id.ItalicsButton);
+		ToggleButton underlinedToggle = (ToggleButton) findViewById(R.id.UnderlineButton);
+		ToggleButton leftButton = (ToggleButton) findViewById(R.id.leftButton);
+		ToggleButton rigButton = (ToggleButton) findViewById(R.id.rightButton);
+		ToggleButton centerButton = (ToggleButton) findViewById(R.id.centerButton);
+		final Button colorBtn  = (Button) findViewById(R.id.colorBtn);
+		
+
+//		View coolButton = findViewById(R.id.CoolButton);
+//		View cryButton = findViewById(R.id.CryButton);
+
+		Button clearButton = (Button) findViewById(R.id.ClearButton);
+
+		dwEdit = (DroidWriterEditText) findViewById(R.id.DwEdit);
+//		dwEdit.setImageGetter(new Html.ImageGetter() {
+//			@Override
+//			public Drawable getDrawable(String source) {
+//				Drawable drawable = null;
+//
+//				try {
+//					if (source.equals("smiley_cool.gif")) {
+//						drawable = getResources().getDrawable(R.drawable.smiley_cool);
+//					} else if (source.equals("smiley_cry.gif")) {
+//						drawable = getResources().getDrawable(R.drawable.smiley_cry);
+//					} else {
+//						drawable = null;
+//					}
+//
+//					// Important
+//					if (drawable != null) {
+//						drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+//					}
+//				} catch (Exception e) {
+//					Log.e("DroidWriterTestActivity", "Failed to load inline image!");
+//				}
+//				return drawable;
+//			}
+//		});
+		dwEdit.setSingleLine(false);
+		dwEdit.setMinLines(10);
+		dwEdit.setBoldToggleButton(boldToggle);
+		dwEdit.setItalicsToggleButton(italicsToggle);
+		dwEdit.setUnderlineToggleButton(underlinedToggle);
+		dwEdit.setLeftToggleButton(leftButton);
+		dwEdit.setCenterToggleButton(centerButton);
+		dwEdit.setRightToggleButton(rigButton);
+//		dwEdit.setImageInsertButton(coolButton, "smiley_cool.gif");
+//		dwEdit.setImageInsertButton(cryButton, "smiley_cry.gif");
+
+		//dwEdit.setClearButton(clearButton);
+		clearButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Toast.makeText(MainActivity.this, dwEdit.getTextHTML(), Toast.LENGTH_LONG).show();
+			}
+		});
+		
+		colorBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				onClickColorPickerDialog(v);
+			}
+		});
+	}
+	
+	public void onClickColorPickerDialog(final View v) {
+				
+		final ColorPickerDialog colorDialog = new ColorPickerDialog(this, color);
+		
+		colorDialog.setAlphaSliderVisible(true);
+		colorDialog.setTitle("Pick a Color!");
+		
+		colorDialog.setButton(DialogInterface.BUTTON_POSITIVE, getString(android.R.string.ok), new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				Toast.makeText(MainActivity.this, "Selected Color: " + colorToHexString(colorDialog.getColor()), Toast.LENGTH_LONG).show();
+							
+				//Save the value in our preferences.
+				color = colorDialog.getColor();
+				v.setBackgroundColor(color);
+				dwEdit.colorChange(color);
+			}
+		});
+		
+		colorDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getString(android.R.string.cancel), new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				//Nothing to do here.
+			}
+		});
+		
+		colorDialog.show();
+	}
+	
+	private String colorToHexString(int color) {
+		return String.format("#%06X", 0xFFFFFFFF & color);
+	}
+	
+
+	
+}
